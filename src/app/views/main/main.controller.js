d6b2394afee12af62c57a08a2c6f33071b73bfe3
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-06-19 13:02:58 +0700

    Show Card

--- src/app/views/main/main.controller.js
+++ src/app/views/main/main.controller.js
@@ -1,7 +1,16 @@
 export class MainController {
-  constructor () {
+  constructor (AppConstant) {
     'ngInject';
 
-    this.vocabs = ['1', '2', '3'];
+    let ls = JSON.parse(localStorage.getItem('vocablist'));
+    this.vocablist = ls ? ls : AppConstant.initialVocabList;
+    this.size = this.vocablist.length;
+    this.showIndex = 0;
+    this.vocab = this.vocablist[this.showIndex];
+  }
+
+  next() {
+    this.showIndex = (this.showIndex + 1) % this.size;
+    this.vocab = this.vocablist[this.showIndex];
   }
 }
