b20efabecfe6eaad550a47719d5b230792edcd20
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-06-25 23:52:40 +0700

    Filter checked word

--- src/app/views/main/main.controller.js
+++ src/app/views/main/main.controller.js
@@ -4,6 +4,7 @@ export class MainController {
 
     let ls = JSON.parse(localStorage.getItem('vocablist'));
     this.vocablist = ls ? ls : AppConstant.initialVocabList;
+    this.vocablist = _.filter(this.vocablist, (o) => { return !o.show; });
     this.size = this.vocablist.length;
     this.showIndex = 0;
     this.showWord = this.vocablist[this.showIndex].word;
